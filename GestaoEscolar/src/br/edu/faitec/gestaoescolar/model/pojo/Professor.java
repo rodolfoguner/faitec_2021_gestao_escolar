c562ce47877087e44d01824af4987d4df51c076d
Rodolfo <rodolfo@pdsolucoes.com.br>
2021-11-22 21:27:41 -0300

    fix: creating an generic classe to improve OO concepts

--- GestaoEscolar/src/br/edu/faitec/gestaoescolar/model/pojo/Professor.java
+++ GestaoEscolar/src/br/edu/faitec/gestaoescolar/model/pojo/Professor.java
@@ -2,6 +2,6 @@ package br.edu.faitec.gestaoescolar.model.pojo;
 
 
 // Classe que contém os atributos e as regras de negócio do sistema
-public class Professor extends Usuario{
+public class Professor extends Colaborador{
 
 }
